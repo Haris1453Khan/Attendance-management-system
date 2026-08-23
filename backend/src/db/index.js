afe46e1ea2d8d25c551f1f9e49650adab0b540fb
Muhammad Haris Khan <hk8998777@gmail.com>
2026-08-24 01:31:05 +0500

    Optimize MongoDB connection for serverless

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,14 +1,20 @@
 import mongoose from 'mongoose';
 import {DB_NAME} from '../constants.js';
 
+let isConnected = false;
+
 const connectDB = async () => {
+    if (isConnected) {
+        return;
+    }
     try{
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        isConnected = !!connectionInstance.connections[0].readyState;
         console.log(`Data Base Connect Successfully **** with: ${connectionInstance.connection.name}`)
     }
     catch(error){
          console.error("Data base connection failed !!!!! ", error);
-         exit(1);
+         process.exit(1);
     }
 }
 
